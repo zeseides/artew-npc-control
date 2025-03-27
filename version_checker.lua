29a9c9167fcb41f6161ef7f44787f4ecc950b2e6
J.Artew <84376192+DeliEmir@users.noreply.github.com>
2025-03-28 02:58:33 +0300

    new update

--- version_checker.lua
+++ version_checker.lua
@@ -1,32 +1,16 @@
 local currentVersion = GetResourceMetadata(GetCurrentResourceName(), 'version', 0)
 local resourceName = GetCurrentResourceName()
 
-print('^2[' .. resourceName .. '] Version checker başlatılıyor...^7')
-print('^2[' .. resourceName .. '] Mevcut versiyon: ' .. currentVersion .. '^7')
-
 local function checkVersion()
-    print('^2[' .. resourceName .. '] GitHub\'dan versiyon kontrol ediliyor...^7')
-    
     PerformHttpRequest('https://raw.githubusercontent.com/JArtew/artew-npc-control/main/fxmanifest.lua', function(err, text, headers)
-        print('^2[' .. resourceName .. '] HTTP Response Code: ' .. tostring(err) .. '^7')
-        
         if err ~= 200 then
-            print('^1[' .. resourceName .. '] Version kontrol edilemedi! Hata kodu: ' .. tostring(err) .. '^7')
-            return
-        end
-        
-        if text then
-            print('^2[' .. resourceName .. '] GitHub\'dan veri alındı. İçerik uzunluğu: ' .. string.len(text) .. '^7')
-        else
-            print('^1[' .. resourceName .. '] GitHub\'dan veri alınamadı!^7')
+            print('^1[' .. resourceName .. '] Version kontrol edilemedi!^7')
             return
         end
         
         local latestVersion = string.match(text, "version%s*'([^']+)'")
         
         if latestVersion then
-            print('^2[' .. resourceName .. '] GitHub\'daki versiyon: ' .. latestVersion .. '^7')
-            
             if currentVersion ~= latestVersion then
                 print('^3╔════════════════════════════════════════════╗^7')
                 print('^3║            GÜNCELLEME MEVCUT!              ║^7')
@@ -38,9 +22,6 @@ local function checkVersion()
             else
                 print('^2[' .. resourceName .. '] Script güncel! (v' .. currentVersion .. ')^7')
             end
-        else
-            print('^1[' .. resourceName .. '] GitHub dosyasında version bilgisi bulunamadı!^7')
-            print('^1[' .. resourceName .. '] Alınan içerik: ' .. text .. '^7')
         end
     end)
 end
